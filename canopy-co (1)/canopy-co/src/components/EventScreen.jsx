698ecd96aed644bbc73d51408f02c8e4655a0214
Sophie <sophia.l.edington@gmail.com>
2026-09-19 14:12:29 -0400

    be gone

--- canopy-co (1)/canopy-co/src/components/EventScreen.jsx
+++
@@ -1,9 +0,0 @@
-export default function EventScreen({ event, onAcknowledge }) {
-  return (
-    <div className="event-screen">
-      <h2>Meanwhile...</h2>
-      <p>{event.text}</p>
-      <button onClick={onAcknowledge}>Continue</button>
-    </div>
-  )
-}
